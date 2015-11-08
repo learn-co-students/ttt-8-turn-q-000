3e99eff30b60ade66caba29734e168c5cb9ea35a
Shanetou <shanemcquerter@gmail.com>
2015-11-08 09:45:39 -0800

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.split
+  if valid_move?(board, position)
+    move(board, position, token = "X")
+  else
+    turn(board)
+  end
+end
+
+
+def move(board, position, token = "X")
+  board[position.to_i - 1] = token
+end
+
+
+def valid_move?(board, position)
+  position = position.to_i
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+
+def position_taken?(board, position)
+  taken = nil
+  if (board[position] == " " || board[position] == "" || board[position] == nil)
+    taken = false
+  elsif board[position] == "X" || board[position] == "O"
+    taken = true
+  end
+  taken
+end
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
