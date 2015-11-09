0d3174d3959843f633ef53053a197e12455d0990
shmulim <shmuel.markel@gmail.com>
2015-11-08 18:45:58 -0800

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,8 +3,11 @@ def turn(board)
   position = gets.strip
 
   until valid_move?(board, position)
-    puts "Please enter 1-9:"
-    position = gets.strip
+    #puts "Please enter 1-9:"
+    #position = gets.strip
+    
+    #using recursion 
+    turn(board)
   end
 
     move(board, position, "X")
