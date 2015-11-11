a76ec5c6ade77df2d2ad975c4a9dab96aae98a95
JayThaGreat <maskdbandit@gmail.com>
2015-11-11 01:32:00 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move(board,pos)
+def valid_move?(board,pos)
     posNum = pos.to_i
   if position_taken?(board,posNum)==false && posNum.between?(1,9)
     true
@@ -15,8 +15,18 @@ def valid_move(board,pos)
   end
 end
 
+def position_taken?(board,pos)
+  posNum = pos.to_i
+  posNum = posNum-1
+  if board[posNum]=="" || board[posNum] ==" " || board[posNum] == nil
+    return false
+  else
+    return true
+    end
+end
+
 def move(board,pos,char = "X")
-    pos = cell.to_i
+    pos = pos.to_i
   pos = pos -1
   board[pos] = char
 
@@ -25,4 +35,14 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  pos = gets.strip
+  if valid_move?(board,pos) == true
+    move(board,pos)#,char)
+  else
+  #until valid_move?(board,pos) == true
+  puts "Please enter 1-9:"
+  pos = gets.strip
+ end
+display_board(board)
 end
+#end
