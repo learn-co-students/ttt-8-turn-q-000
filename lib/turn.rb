dd3651e05f72b3bcc63bb6f1744f2d4febab861b
mackdalton <mackdalton@gmail.com>
2015-11-27 14:34:08 -0500

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,16 +1,18 @@
 
-board = ["","","","","","","","",""]
+
+def welcome
+  puts "Welcome to Tic Tac Toe!"
+end
 
 def display_board(board) 
-    puts "  #{board[0]} |  #{board[1]} |  #{board[2]} "
-    puts "-----------"
-    puts "  #{board[3]} |  #{board[4]} |  #{board[5]} "
-    puts "-----------"
-    puts "  #{board[6]} |  #{board[7]} |  #{board[8]} "
-end
-display_board(board)
 
-#valid_move?
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def valid_move?(board, position)
   move = position.to_i - 1
   if move.between?(0 , 8) 
@@ -32,14 +34,21 @@ def position_taken?(board, position)
   end
 end
 
-#move
 
-def move(board, location, value = "X")
-  board[location.to_i-1] = value
 
+def move(board, position, value = "X")
+  board[position.to_i - 1] = value
+  
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  
+  position = gets.strip
+  if valid_move?(board, position)
+    move(board, position, value = "X")
+  else
+      puts "Please enter 1-9:"
+      position = gets.strip
+  end
+    display_board(board)
 end
