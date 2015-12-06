309d827ec4e76651183bc46651aa49b7515ca869
macgeek30 <mike25ko@gmail.com>
2015-12-06 16:36:08 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,41 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  while !valid_move?(board, input)
+    puts "Please enter 1-9:"
+    input = gets.strip
+  end
+  move(board, input)
+  display_board(board)
+end
+
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+end
+
+def valid_move?(board, position)
+  if position == "invalid"
+    return false
+  end
+  position = position.to_i
+  if position - 1 > 8
+    return false
+  elsif position_taken?(board, position - 1)
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, position)
+  (board[position] == " " || board[position] == "" || board[position] == nil)?
+  false:true
+end
+
+def move(board, position, mark="X")
+  board[(position.to_i)-1] = mark
+end
