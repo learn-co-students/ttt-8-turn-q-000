b2cde1a98d2fdcade7ae335e3289ed8d96050059
fiveinfinity <jlachance1@gmail.com>
2015-12-09 22:30:52 -0600

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,37 +8,58 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   position = gets.strip
+#   position = position.to_i
+
+#   if(valid_move?(board, position))
+#     move(board, position, "X")
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
+# end
+
+# #CORRECT
+# def valid_move?(board, position)
+#   position = position.to_i
+#   position = position -= 1
+
+#   if position.between?(0, 8) != true
+#     return false
+#   elsif board[position] == " " || board[position] == "" || board[position] == nil
+#     return true
+#   else
+#     return false
+#   end
+
+# end
+
+
 def turn(board)
   puts "Please enter 1-9:"
-  position = gets.strip
-  position = position.to_i
+  location = gets.strip
 
-  if(valid_move?(board, position))
-    move(board, position, "X")
-    display_board(board)
+  if valid_move?(board, location)
+    move(board, location, "X")
   else
     turn(board)
   end
 end
 
-#CORRECT
-def valid_move?(board, position)
-  position = position.to_i
-  position = position -= 1
-
-  if position.between?(0, 8) != true
-    return false
-  elsif board[position] == " " || board[position] == "" || board[position] == nil
-    return true
-  else
-    return false
-  end
-
+def move(board, location, token)
+  location = location.to_i-1
+  board[location] = token
+  display_board(board)
 end
 
-def move(board, position, token)
-  position -= 1
-  board[position] = token
+def valid_move?(board, location)
+  location = location.to_i-1
+  location.between?(0,8) && !position_taken?(board, location)
 end
 
-
+#position taken if true, not taken if false.
+def position_taken?(board, location)
+  !(board[location].nil? || board[location] == " ")
+end
