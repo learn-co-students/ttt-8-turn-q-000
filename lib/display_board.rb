d8fa915327a20036932a3fad50fc932b968f03e7
mystycs <mystycs@gmail.com>
2015-12-30 18:17:18 +0000

    Done.

--- lib/display_board.rb
+++
@@ -1,9 +0,0 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-def display_board(moves_array)
-  puts " #{moves_array[0]} | #{moves_array[1]} | #{moves_array[2]} "
-  puts "-----------"
-  puts " #{moves_array[3]} | #{moves_array[4]} | #{moves_array[5]} "
-  puts "-----------"
-  puts " #{moves_array[6]} | #{moves_array[7]} | #{moves_array[8]} "
-end
--- lib/move.rb
+++
@@ -1,5 +0,0 @@
-def move(array, position, value = "X")
-  position = position.to_i
-  array[position-1] = value
-  display_board(array)
-end
