5d05bab5bccf4f5530ba22ba35735b8a2dcdb8ee
rambllah <adam.bramhall@gmail.com>
2016-01-02 21:18:24 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,3 @@
-
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,18 +7,17 @@ def display_board(board)
 end
 
 def valid_move?(board, position)
-  if position.to_i.between?(1,9) && (position_taken?(board, position.to_i) == false) # if the position is a space on the board between one and nine AND the position has not already been taken, it is a valid move
+  if position.to_i.between?(1,9) && (position_taken?(board, position.to_i) == false)
       true
-  else # otherwise, the move is invalid
+  else
       false
     end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
-  if board[position.to_i-1] == " " || board[position.to_i-1] == "" || board[position.to_i-1] == nil # if the position is empty like so " " or like so "", or if the position has a value of nil, it has not been taken
+  if board[position.to_i-1] == " " || board[position.to_i-1] == "" || board[position.to_i-1] == nil
     false
-  else # otherwise, it is unavailable
+  else
     true
   end
 end
