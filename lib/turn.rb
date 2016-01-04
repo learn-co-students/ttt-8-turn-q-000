512f42099483a550e1679c1ebaa789c29efaf65a
Joshua Azemoh <buzzme46@gmail.com>
2016-01-04 02:29:42 +0000

    Done

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+
+# Accept position and make move if valid
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+
+  until valid_move?(board, position)
+    puts "Invalid Move! Try again."
+    position = gets.strip
+  end
+
+  move(board, position)
+  display_board(board)
+end
+
+# Place token in correct index in the board array
+def move(board, position, token = "X")
+  index = position.to_i - 1
+  board[index] = token
+end
+
+# Check if a move is valid
+def valid_move?(board, position)
+  index = position.to_i - 1
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+# Check if a position is already filled
+def position_taken?(board, position)
+  board[position] == "X" || board[position] == "O"
+end
+
+# Print the board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
