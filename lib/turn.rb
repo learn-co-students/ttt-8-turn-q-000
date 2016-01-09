41a49a65118e0336103c97617f61aeb305363677
reifnotreef <bridgerrhammond@gmail.com>
2016-01-09 19:52:32 +0000

    nitrous-backup at Sat Jan  9 19:52:32 UTC 2016

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,10 @@
 #Def Display Board - using previous lesson
 def display_board(board)
-  puts  " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts  "-----------"
-  puts  " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts  "-----------"
-  puts  " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def position_taken?(board, position) #Define Postion Taken?
