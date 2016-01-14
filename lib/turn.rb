05a1286ce0c60b5cb43247f0765436740516c174
sameera91 <sbachoti91@gmail.com>
2016-01-14 04:06:02 +0000

    nitrous-backup at Thu Jan 14 04:06:02 UTC 2016

--- lib/turn.rb
+++ lib/turn.rb
@@ -51,3 +51,13 @@ def turn(board)
     end
   end
 end
+
+def turn_count(board)
+  count = 0
+
+  board.each do|position|
+    if position == "X" || position == "O"
+      count += 1
+    end
+  end
+end
