0c7b75bc32c2f38dbee7c13cb41a22fb2ebf5184
ffrostt <ffrostt@gmail.com>
2016-01-13 23:39:01 -0500

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def position_taken?(board, position)
+  position = position.to_i - 1
+  board[position] == "X" || board[position] == "O" ? true : false
+end
+
+def valid_move?(board, position)
+  position = position.to_i - 1
+  !position_taken?(board, position) && position == (0..8) ? true : false
+end
+
+def move(board, position, value = "X")
+  position = position.to_i - 1
+  board[position] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  position = gets.strip
+  if valid_move?(board, position)
+    move(board, position)
+    display_board(board)
+  else
+    puts "Invalid, please try again"
+  end
+end
+
+#ask for input
+#get input
+#if input is valid
+#  make the move for input
+#else
+#  ask for input again until you get a valid input
+#end
+#show the board
