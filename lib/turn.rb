3aeaa24eb683713a9650099120473b2acb263e3a
Jaken <jake@devpotion.com>
2016-03-08 21:31:30 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,8 +7,36 @@ def display_board(board)
 end
 
 def valid_move?(board, position)
+  position = position.to_i - 1
+    if position == -1
+      return false
+    else
+     if !position_taken?(board, position) 
+       return true
+     else
+       return false
+     end
+    end
+end
+def position_taken?(board, position)
+    if board[position] == " "
+      return false
+    else
+     return true
+    end
+end
+
+def move(board, position, player = "X")
+  board[position.to_i - 1] = player
+  display_board(board)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
+  position = gets
+  if (valid_move?(board, position))
+     move(board, position)
+  else
+    turn(board)
+  end
 end
