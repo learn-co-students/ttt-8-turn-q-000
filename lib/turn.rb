c0bca4843ab148eef3500275548d2e98701cf75b
John Hillegass <hillegassdev@gmail.com>
2016-03-18 21:02:55 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,37 +6,34 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
-
 def move(board, input, value="X")
   input = input.to_i-1
   board[input] = value
   display_board(board)
 end
 
-board = [" "," "," "," "," "," "," "," "," "]
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, input)
   playerToken = "X"
   if board[input-1].include?(playerToken)
     puts "This position is already taken, please enter another value"
     turn(board)
-    return false
-   else
+  else
     move(board, input, "X")
   end
-  return true
+  return false
 end
 
 def valid_move?(board, input)
   input =  input.to_i
   if input.between?(1, 9)
-      position_taken?(board, input)
-    else
+     if position_taken?(board, input)
+       return false
+     end
+  else
       puts "This number is not between 1 and 9"
       turn(board)
-    end
+  end
+  return true
 end
 
 def turn(board)
