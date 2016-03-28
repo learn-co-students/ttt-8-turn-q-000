bf3cda1434893e5d7f444d53a973c2ed47abd6b2
Llubomir Stanojlovic <candid.llama@gmail.com>
2016-03-28 22:54:46 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,6 +24,7 @@ def position_taken?(board, position)
   if (board[position.to_i]==" " || board[position.to_i]=="" || board[position.to_i]==nil)
     false
   elsif (board[position.to_i]=="X" || board[position.to_i]=="O")
+    true
   end
 end
 
