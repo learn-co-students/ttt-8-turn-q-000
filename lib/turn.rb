958c1848f7ca2c62794cba2b2996686250c0f6b8
dpreli <dave.preli@gmail.com>
2016-03-29 21:03:03 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,4 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-position = gets.strip
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -31,6 +30,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  position = gets.strip
   if valid_move?(board, position) == true
     move(board, position, value = "X")
   elsif valid_move?(board, position) == false
