f6eec86c7f63131d169a2861eb87145c417cb4de
Traci7822 <tracir@gmail.com>
2016-04-17 14:58:36 -0600

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,36 +6,30 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
-
 def valid_move?(board, position)
-  move = position.to_i - 1
-  if move.between?(0,8) && !position_taken?(board, move)
+  if position.to_i.between?(1,9) 
+    if !position_taken?(board, position.to_i - 1)
     true
-  else   
-    false
 end
 end
-
-def move(board, location, character = "X")
-position = location.to_i - 1
-update_array(board, position, character)
 end
 
 
 def turn(board)
 puts "Please enter 1-9:"
-position = gets.to_i - 1
-if !valid_move?(board, position)
-  loop do
-    puts "Please enter 1-9:"
-    position = gets.to_i - 1
- until position.between?(0,8)
-      end
-    end
+input = gets.strip
+if valid_move?(board, input)
+  move(board, input)
+  else
+    turn(board)
+  end
+  display_board(board)
 end
+
+
+def move(board, position, character = "X")
+board[position.to_i - 1] = character
 end
- 
 
 def position_taken? (board, position)
 if board[position] == " " || board[position] == "" || board[position] == nil
@@ -45,6 +39,6 @@ elsif board[position] == "X" || board[position] == "O"
 end
 end
 
-def update_array(array, position, value)
-  array[position] = value
+def update_array(array, position, character)
+  return array[position.to_i] = character
 end
