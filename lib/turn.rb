f8b163099b7a42095383464dc1058d07131bf610
anitajordan <anita.jordan21@gmail.com>
2016-05-04 11:59:44 -0700

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+def display_board(board)
+  print " #{board[0]} ", "|", " #{board[1]} ", "|", " #{board[2]} \n"
+  puts "-----------"
+  print " #{board[3]} ", "|", " #{board[4]} ", "|", " #{board[5]} \n"
+  puts "-----------"
+  print " #{board[6]} ", "|", " #{board[7]} ", "|", " #{board[8]} \n"
+end
+   
