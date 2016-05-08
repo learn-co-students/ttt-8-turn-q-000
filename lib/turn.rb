3ce9f38b58fda8ffde624a9ee0f592999c05325a
jinzh3ng <jinzheng87@gmail.com>
2016-05-08 14:27:54 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,23 +9,27 @@ end
 
 #position_taken?
 def position_taken?(board, position)
+    # if position on the board is empty, or nil then the board is not taken
     if board[position] == "" || board[position] == " " || board[position] == nil
         return false
-    else board[position] == "X" || board[position] == "O"
+    # else if the board is not empty, then the position is taken
+    else 
         return true
     end
 end
 
 #valid_move?
 def valid_move?(board, position)
+  # translate input (1-9) into array indexing (0-8) on the board
     input = position.to_i - 1
+    # if the position is not taken, and the array indexing is between 0 & 8 
+    # then it's a valid move, or else it's not a valid move
     if !position_taken?(board, input) && input.between?(0,8)
         return true
     else
         return false
     end
 end
-
 #make a move
 def move(board, input, n = "X")
     if valid_move?(board, input) && (n == "X" || n == "O")
