1ad496f9b928e0d4e38a66bdacf89ae68af9b872
Rj <ray.a.albright@gmail.com>
2016-06-13 14:07:20 -0500

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,16 +22,12 @@ def valid_move?(board, index)
   index.between?(0, board.length) && !position_taken?(board, index)
 end
 
-# def valid_input?(input)
-#   input.downcase == input
-# end
-
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, 'X') if !position_taken?(board, index)
+    move(board, index)
     display_board(board)
   else
     turn(board)
