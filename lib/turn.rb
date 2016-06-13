7b3f2d6f09749029504501656fc8bb3dee95a620
Daniel Rosen <technite@gmail.com>
2016-06-13 04:53:08 +0000

    Improved solution with recursion

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,14 +36,15 @@ end
 
 #turn
 def turn(board)
+  puts "Welcome to Tic Tac Toe!"
+  display_board(board)
   puts "Please enter 1-9:"
   input = gets.strip()
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
-    puts "Please enter another input"
-    input = gets.strip()
+    turn(board)
   end
-  display_board(board)
 end
