822970a9a7ca71cb08cb0e775c347dcc200ac1a5
Errol Rodrigues <errolrodrigues@hotmail.co.uk>
2016-06-17 13:29:51 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,48 +6,36 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
 
-
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
 
 def turn(board)
-  display_board(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  input_to_index(input)
-
-  if valid_move?(board,input)
-       move(board, input)
-       display_board(board)
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
   else
-      puts "Please enter 1-9:"
-      input = gets.strip
+    turn(board)
   end
 end
 
-
-
-def input_to_index(input)
-  user_input = input.to_i
-  index = user_input - 1
+def position_taken?(board, index)
+  board[index] != " "
 end
 
-
-def valid_move?(board,input)
-  user_input = input.to_i
-  index = user_input - 1
-  move = ""
-
-  if (index >= 0 && index <= 8) #&& board[index] == " " || board[index] == ""
-    move = true
-  elsif (index < 0 || index > 8) #&& board[index] == "X" || board[index] == "O"
-    move = false
-  else move = false
-  end
-end
-
-def move (board, input,value = "X")
-  user_input = input.to_i
-  index = user_input - 1
-  board[index] = value
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
