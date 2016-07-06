cee6ae63fe66f50cd4f5b2458bb084d9b5ab52a1
itsthecheat <llaurenb@gmail.com>
2016-07-06 19:38:03 -0400

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+board = [" " , " " , " " , " " , " " , " " , " " , " " , " "]
+
+def display_board(board)
+
+  line = "-----------"
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts line
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts line
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
