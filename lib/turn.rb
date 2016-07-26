fd22ed16816decf506883ae703e9cb809c72c8ee
raj <j.g.malhotra@gmail.com>
2016-07-26 10:37:43 +0530

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0, 9) && !position_taken?(board, index) 
+  if index.between?(0, 8) && !position_taken?(board, index) 
         return true
     else
         return false 
