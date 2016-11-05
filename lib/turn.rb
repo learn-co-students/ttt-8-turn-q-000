e469c2e7c81788068277a1a8d1c97a9f049783bb
ExhoSix <donnalalgoso@gmail.com>
2016-11-05 13:04:18 -0400

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,18 +1,18 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts "#{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
-  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " ----------- "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " ----------- "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(input)
   input.to_i-1
 end
 
-def valid_move(board, index)
+def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0,8)
     true
   else 
@@ -21,19 +21,19 @@ def valid_move(board, index)
 end
 
 def position_taken?(board, index)
-  if board[index] == "x" || board[index] == "o"
+  if board[index] == "X" || board[index] == "O"
     true
   else
     false
   end
 end
 
-def move(board, position, charatcer = "x")
+def move(board, position, charatcer = "X")
   board[position] = charatcer
 end
 
 def turn(board)
-  puts "Please enter 1-9."
+  puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
